93060c77a808ee7ccbde9f269f6a1540ba9d1afe
Mahbubur Rahman Manik <mrmanik.kuet@gmail.com>
2020-08-29 14:08:07 +0600

    Update 03. Longest Substring Without Repeating Characters.js

--- 03. Longest Substring Without Repeating Characters.js
+++ 03. Longest Substring Without Repeating Characters.js
@@ -2,15 +2,18 @@
  * @param {string} s
  * @return {number}
  */
+
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
 var lengthOfLongestSubstring = function(s) {
     if (s.length === 0) 
-		return 0; 
+	return 0; 
 		
     var start = 0, maxLen = 1;   
     for (var j = 0; j < s.length; j++) {
         var c = s.charAt(j);
       
-		var substr= s.substring(start, j);
+	var substr= s.substring(start, j);
         var idx = substr.lastIndexOf(c);
         if (idx > -1) {
             start = start + idx + 1;
